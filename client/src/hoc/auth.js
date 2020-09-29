3688bda9315c403142900ba914b7e63a2c69d8ea
EunsungAhn <esahn97@gmail.com>
2020-09-29 12:02:50 +0900

    isAdmin 해결은 못했지만 완강함 ㅠㅠ

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import Axios from "axios";
+// import Axios from "axios";
 import { useDispatch } from "react-redux";
 import { auth } from "../_actions/user_action";
 
@@ -13,19 +13,19 @@ export default function (SpecificComponent, option, adminRoute = null) {
     useEffect(() => {
       dispatch(auth()).then((response) => {
         console.log(response);
-        // // 로그인하지 않은 상태
-        // if (!response.payload.isAuth) {
-        //   if (option) {
-        //     props.history.push("/login");
-        //   }
-        // } else {
-        //   // 로그인한 상태
-        //   if (adminRoute && !response.payload.isAdmin) {
-        //     props.history.push("/");
-        //   } else {
-        //     if (option === false) props.history.push("/");
-        //   }
-        // }
+        // 로그인하지 않은 상태
+        if (!response.payload.isAuth) {
+          if (option) {
+            props.history.push("/login");
+          }
+        } else {
+          // 로그인한 상태
+          if (adminRoute && !response.payload.isAdmin) {
+            props.history.push("/");
+          } else {
+            if (option === false) props.history.push("/");
+          }
+        }
       });
     }, []);
 
